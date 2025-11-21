a908679feb512e5a0559df9468749296f1dba4b0
MARIO JUNIOR CASTILLO GARCIA <84743913+adoscasas10@users.noreply.github.com>
2025-11-21 11:09:48 -0500

    Update login.php

--- App/View/Login/login.php
+++ App/View/Login/login.php
@@ -17,7 +17,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logins</title>
+    <title>Login</title>
     <?php include '../Layout/head.php'; ?>
 </head>
 
